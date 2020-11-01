4c9ae5403abfe987c25f50d5611148cf4f169a83
MacBook Pro <Macbookpro@MacBooks-MacBook-Pro-5.local>
2020-11-01 10:32:23 +0700

    todo

---
+++ .history/src/components/add_20201101083428.js
@@ -0,0 +1,27 @@
+import React,{useState} from 'react'
+import Button from 'react-bootstrap/Button'
+export default function Add({value,onCheck})
+{
+    // const {value}=props;
+    const [addValue,setAddValue]=useState('')
+    function handleChange(e){
+        if(e.target.value!==""){
+            setAddValue(e.target.value)
+        }
+        // console.log(e.target.value)
+    }
+    function onClickHandle(){
+        setAddValue('');
+    }
+    function handleSub(e){
+        e.preventDefault();
+        console.log(addValue)
+        onCheck && typeof onCheck === 'function' && onCheck(addValue)
+    }
+    return(
+        <form style={{display:'flex'}} className="mt-3 mb-3" onSubmit={handleSub}>
+            <input style={{width:'100%'}} type="text" value={addValue} onChange={handleChange}/>
+             <Button onClick={()=>{onClickHandle()}} className='ml-1' variant="danger">Clear</Button>
+        </form>
+    )
+}
